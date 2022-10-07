71fa70c6bd02d6ca383999a67afbd70e8cb2af0e
Otti Daniel <ottidan20@gmail.com>
2022-10-07 17:05:10 +0100

    wip

---
+++ Basic Algorithm Scripting/FIndersKeepers.js
@@ -0,0 +1,24 @@
+// Create a function that looks through an array arr
+// and returns the first element in it that passes a 'truth test'.
+// This means that given an element x,
+// the 'truth test' is passed if func(x) is true.
+// If no element passes the test, return undefined.
+
+// loop
+function findElement(arr, func) {
+  let num = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)) {
+      return num;
+    }
+  }
+
+  return undefined;
+}
+
+// Using find
+function findElement(arr, func) {
+  return arr.find(func);
+}
